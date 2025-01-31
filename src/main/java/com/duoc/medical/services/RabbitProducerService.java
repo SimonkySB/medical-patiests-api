8b04cd4690f66ee07770ee8ecbc3f6218bcb2927
simonky <simonasb19@gmail.com>
2025-01-31 08:16:37 -0300

    add rabbitmq

---
+++ src/main/java/com/duoc/medical/services/RabbitProducerService.java
@@ -0,0 +1,24 @@
+package com.duoc.medical.services;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitProducerService {
+    @Autowired 
+    private RabbitTemplate rabbitTemplate;
+
+    public void sendAlert(String message)
+    {
+        rabbitTemplate.convertAndSend(
+            "exchange-rabbit", "routing-key-alert", message);
+    }
+
+
+    public void sendSummary(String message)
+    {
+        rabbitTemplate.convertAndSend(
+            "exchange-rabbit", "routing-key-summary", message);
+    }
+}
